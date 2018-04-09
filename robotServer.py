135a3bb08dc64b20bc3ed16b75081cc225e8d844
jzbass25 <11434311+jzbass25@users.noreply.github.com>
2018-04-09 00:04:49 -0400

    Update robotServer.py

--- robotServer.py
+++ robotServer.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
-import logging
+#import logging
+import pickle
 #logging.basicConfig(level=logging.DEBUG)
+from PIL import Image
+import io
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -12,9 +15,19 @@ def index():
     return render_template('index.html')
 
 @socketio.on('Mov')
-def test_message(message):
+def on_message(message):
     print(message)
-    socketio.emit('movement', message)
+    socketio.emit('move', message)
+
+@socketio.on('vid')
+def on_vid(frame):
+    image_stream = io.BytesIO()
+    image_stream.write(pickle.loads(frame))
+    image_stream.seek(0)
+    t = Image.open(image_stream)
+    print('Image Received')
+    t.save("/home/pi/robots/static/test.jpg", "JPEG")
+    socketio.emit('frame', '')
 
 if __name__ == '__main__':
     socketio.run(app)
